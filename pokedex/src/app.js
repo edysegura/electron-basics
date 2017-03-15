b0ea12e42c4c9e51cb3597848c973d3287c50a8b
Edy Segura <ednardo@inatel.br>
2017-03-15 12:27:29 -0300

    Start pokedex example

---
+++ pokedex/src/app.js
@@ -0,0 +1,29 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+const url = require('url')
+
+let win = null
+
+function createWindow () {
+    win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        transparent: true, 
+        frame: false
+    })
+
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file',
+        slashes: true
+    }))
+
+    win.on('closed', () => {
+        win = null
+    })
+}
+
+app.on('ready', createWindow)
+app.on('window-all-closed', () => {
+    app.quit()
+})
