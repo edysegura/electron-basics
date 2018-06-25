b5661a05ea866cc2470f3fa2aae405bcc6af83e8
Edy Segura <edysegura@gmail.com>
2018-06-25 15:18:13 -0300

    Change code formatting to basic example

--- quick-start/index.js
+++ quick-start/index.js
@@ -1,14 +1,14 @@
 const packageJson = require('./package.json')
 
 let versions = {
-    "node": process.versions.node,
-    "chrome": process.versions.chrome,
-    "electron": process.versions.electron,
-    "appInfo": `${packageJson.name}@${packageJson.version}`
+  node: process.versions.node,
+  chrome: process.versions.chrome,
+  electron: process.versions.electron,
+  appInfo: `${packageJson.name}@${packageJson.version}`
 }
 
-function showVersion (item) {
-    document.getElementById(item).innerHTML = versions[item]
+function showVersion(item) {
+  document.getElementById(item).innerHTML = versions[item]
 }
 
 Object.keys(versions).forEach(showVersion)
--- quick-start/main.js
+++ quick-start/main.js
@@ -1,27 +1,29 @@
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow } = require('electron')
 const path = require('path')
 const url = require('url')
 
-let win = null
+let win = undefined
 
-function createWindow () {
-    win = new BrowserWindow({
-        width: 800,
-        height: 600
+function createWindow() {
+  win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadURL(
+    url.format({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file',
+      slashes: true
     })
+  )
 
-    win.loadURL(url.format({
-        pathname: path.join(__dirname, 'index.html'),
-        protocol: 'file',
-        slashes: true
-    }))
-
-    win.on('closed', () => {
-        win = null
-    })
+  win.on('closed', () => {
+    win = undefined
+  })
 }
 
 app.on('ready', createWindow)
 app.on('window-all-closed', () => {
-    app.quit()
+  app.quit()
 })
