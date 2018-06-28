eed2d7737ac03f6cfeb798f37040c0e44314936a
Edy Segura <edysegura@gmail.com>
2018-06-28 17:59:17 -0300

    Refactoring script catch-them-all to use ES6 features

--- pokedex/scripts/catch-them-all.js
+++ pokedex/scripts/catch-them-all.js
@@ -1,41 +1,39 @@
-function catchPokemon(pokemonNumber) {
-  var http = require('http'),
-    fs = require('fs')
+const http = require('http')
+const fs = require('fs')
 
-  var options = {
+function catchPokemon(pokemonNumber) {
+  const options = {
     host: 'assets.pokemon.com',
     port: 80,
-    path: '/assets/cms2/img/pokedex/full/' + pokemonNumber + '.png'
+    path: `/assets/cms2/img/pokedex/full/${pokemonNumber}.png`
   }
 
-  var request = http.get(options, function(res) {
-    if (res.headers['content-type'] != 'image/png') {
-      return null
+  http.get(options, function(response) {
+    if (response.headers['content-type'] != 'image/png') {
+      return undefined
     }
 
     var chunks = []
-    res.on('data', function(chunk) {
-      chunks.push(chunk)
-    })
+    response.on('data', (chunk) => chunks.push(chunk))
 
-    res.on('end', function() {
-      var buffer = Buffer.concat(chunks)
+    response.on('end', () => {
+      const buffer = Buffer.concat(chunks)
       fs.writeFile(
-        'src/images/pokemons/' + pokemonNumber + '.png',
+        `src/images/pokemons/${pokemonNumber}.png`,
         buffer,
-        function(err) {
-          if (err) {
-            console.log('Missed Pokemon #' + pokemonNumber)
+        (error) =>{
+          if (error) {
+            console.log(`Missed Pokemon #${pokemonNumber}`)
             return
           }
-          console.log('Caught Pokemon #' + pokemonNumber)
+          console.log(`Caught Pokemon #${pokemonNumber}`)
         }
       )
     })
   })
 }
 
-for (let index = 1; index < 252; index++) {
+for (let index = 1; index < 255; index++) {
   const pokemonNumber = String(index).padStart(3, '0')
   catchPokemon(pokemonNumber)
 }
